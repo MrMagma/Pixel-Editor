10c78ea72783668cd06524e916a03ec8a4354b5a
Mr. Magma <joshuamagma2013@gmail.com>
2015-12-17 12:19:27 -0600

    Do some work on the store

--- dist/src/Stores/CanvasStore.js
+++ dist/src/Stores/CanvasStore.js
@@ -3,8 +3,21 @@
 var CanvasDispatcher = require("../Dispatcher/CanvasDispatcher.js");
 var EventEmitter = require('events').EventEmitter;
 
-var CanvasStore = {};
+var pixels = [[0]];
+
+var CanvasStore = {
+    resize: function resize(_ref) {
+        var _ref$width = _ref.width;
+        var width = _ref$width === undefined ? pixels.length : _ref$width;
+        var _ref$height = _ref.height;
+        var height = _ref$height === undefined ? pixels[0].length : _ref$height;
+    }
+};
 
 _.extendOwn(CanvasStore, EventEmitter.prototype, {});
 
-CanvasDispatcher.register(function (action) {});
+CanvasDispatcher.register(function (action) {
+    if (CanvasStore[action.type]) {
+        CanvasStore[action.type](action);
+    }
+});
--- src/Stores/CanvasStore.js
+++ src/Stores/CanvasStore.js
@@ -1,14 +1,29 @@
 var CanvasDispatcher = require("../Dispatcher/CanvasDispatcher.js");
 var EventEmitter = require('events').EventEmitter;
 
-var CanvasStore = {};
+var pixels = [[0]];
+
+var events = {
+    resize({width = pixels.length, height = pixels[0].length}) {
+        if (width > 0) {
+            // Set the width of the pixels array
+        }
+        if (height > 0) {
+            // Set the height of the pixels array
+        }
+    }
+};
+
+var CanvasStore = {
+    
+};
 
 _.extendOwn(CanvasStore, EventEmitter.prototype, {
     
 });
 
 CanvasDispatcher.register(function(action) {
-    
-    
-    
+    if (events[action.type]) {
+        events[action.type](action);
+    }
 });
